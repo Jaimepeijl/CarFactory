5a31a4dbe5800e6ac4d793308cb42f6a14c772a7
Jaime.van.der.peijl <jaime.van.der.peijl@ordina.nl>
2023-02-13 11:10:58 +0100

    Initial commit

---
+++ src/test/java/nl/ordina/carfactory/CarFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ordina.carfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
