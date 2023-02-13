448ef950d41ecdd86aabaebc3de4a024d220c516
Jaime.van.der.peijl <jaime.van.der.peijl@ordina.nl>
2023-02-13 14:00:53 +0100

    Added the ComponentScan so it works in LocalHost

--- api/src/main/java/nl.ordina.CarFactory.api/CarFactoryController.java
+++ api/src/main/java/nl.ordina.CarFactory.api/CarFactoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CarFactoryController {
 
-    private final CarFactoryService carFactoryService;
+    public final CarFactoryService carFactoryService;
 
     public CarFactoryController(CarFactoryService carFactoryService) {
         this.carFactoryService = carFactoryService;
--- app/src/main/java/nl.ordina.CarFactory.app/CarFactoryApplication.java
+++ app/src/main/java/nl.ordina.CarFactory.app/CarFactoryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("nl.ordina.CarFactory.*")
 public class CarFactoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarFactoryApplication.class, args);
