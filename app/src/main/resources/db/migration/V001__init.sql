940f43a8d99301b7c54f465c9315a368d1f81427
LarsS968 <124789638+LarsS968@users.noreply.github.com>
2023-03-08 11:28:43 +0100

    Laptop added in DB

--- app/src/main/resources/db/migration/V001__init.sql
+++ app/src/main/resources/db/migration/V001__init.sql
@@ -1,21 +1,32 @@
 CREATE TABLE IF NOT EXISTS phone(
 
-                                         name varchar(20) PRIMARY KEY,
-                                         color varchar(20),
-                                         cameras int,
-                                         stock bigint,
-                                         min_stock bigint,
-                                         max_stock bigint
+                                    name varchar(20) PRIMARY KEY,
+                                    color varchar(20),
+                                    cameras int,
+                                    stock bigint,
+                                    min_stock bigint,
+                                    max_stock bigint
 
 );
 CREATE TABLE IF NOT EXISTS car(
 
-                                    brand varchar(20) PRIMARY KEY,
-                                    model varchar(20),
-                                    colour varchar(20),
-                                    type varchar(20),
-                                    stock bigint,
-                                    min_stock bigint,
-                                    max_stock bigint
+                                  brand varchar(20) PRIMARY KEY,
+                                  model varchar(20),
+                                  colour varchar(20),
+                                  type varchar(20),
+                                  stock bigint,
+                                  min_stock bigint,
+                                  max_stock bigint
+
+);
+CREATE TABLE IF NOT EXISTS laptop(
+
+                                     brand varchar(20),
+                                     model varchar(20) PRIMARY KEY,
+                                     colour varchar(20),
+                                     type varchar(20),
+                                     stock bigint,
+                                     min_stock bigint,
+                                     max_stock bigint
 
 );
