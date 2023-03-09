be23ced6ae2fc63fdc37c20f5daed3806fc99c5c
LarsS968 <124789638+LarsS968@users.noreply.github.com>
2023-03-09 13:41:04 +0100

    improvements

--- repository/src/main/java/nl/ordina/distribution/repository/model/Laptop.java
+++ repository/src/main/java/nl/ordina/distribution/repository/model/Laptop.java
@@ -6,23 +6,30 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import java.util.ArrayList;
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Laptop {
-    private String brand;
+
     @Id
+    @GeneratedValue
+    private UUID laptopId;
+    private String brand;
     private String model;
     private String colour;
     private String type;
     private int stock;
     private int minStock;
     private int maxStock;
-    private int screenInInches;
+    private int screenWidthInInches;
     private String processor;
 
     private static ArrayList<Laptop> laptops = new ArrayList<>();
